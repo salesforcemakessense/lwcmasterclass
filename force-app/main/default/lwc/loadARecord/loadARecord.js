3c91654163e292876e6021bbd073dddc4e6a5ab9
salesforcemakessense <salesforcemakessense@gmail.com>
2025-06-28 10:03:57 +0400

    LDS - load a record snippet

---
+++ force-app/main/default/lwc/loadARecord/loadARecord.js
@@ -0,0 +1,7 @@
+import { LightningElement, api} from 'lwc';
+
+export default class LoadARecord extends LightningElement {
+    @api recordId;
+    objectApiName='Case';
+    fields=['CaseNumber', 'Priority', 'Status','Reason','Type'];
+}
